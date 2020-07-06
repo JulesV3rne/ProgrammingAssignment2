2237ce335bcaa033bb6f536111509007f3b0e610
JulesV3rne <matt438888@gmail.com>
2020-07-06 15:25:03 +1000

    Initial commit. Added body for functions makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+##A pair of functions to create a custom cacheable matrix-type object and
+##to return the cached value if computed and assign it to the object if it isn't.
+##Adapted from the example functions operating on vectors and means
 
-## Write a short comment describing this function
+#Creates a matrix with functions to set and return a cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+##Looks up the stored inverse if it exists. If it doesn't, solves and assigns it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
